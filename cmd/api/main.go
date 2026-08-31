03036be82e676d6d135558857642578e4ff4642d
Sujal Patel <sujal7455@gmail.com>
2026-09-01 00:49:14 +0530

    add dotenv config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,28 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/devsujalpatel/olx-api/internal/config"
 )
 
 func main() {
+	cfg := config.MustLoad()
+  fmt.Println("starting olx server...")
+  
+  // Creating mux or route
 	mux := http.NewServeMux()
 
+	// creating health check route
 	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request){
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`{"status": "ok" }`))
 	})
+	
+	// geting port from e.env
+ 
 
+  // starting the http server
 	 srv := http.Server{
-		Addr: ":8090",
+		Addr: ":" + cfg.Port,
 		Handler: mux,
 		ReadTimeout: time.Second * 10,
 		WriteTimeout: time.Second * 30,
 		IdleTimeout: time.Second * 60,
 	}
-
+	log.Printf("server is listening on %s", srv.Addr)
+	
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("server failed: %v", err)
 	}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/devsujalpatel/olx-api
 
 go 1.27.0
+
+require github.com/joho/godotenv v1.5.1
