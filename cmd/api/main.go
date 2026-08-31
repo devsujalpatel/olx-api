cd50de2fef578194100565f0deca1a039739696d
Sujal Patel <sujal7455@gmail.com>
2026-08-31 15:09:27 +0530

    custom mux and healthz route

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+)
 
 func main() {
-	fmt.Println("olx-api server is running...")
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status": "ok" }`))
+	})
+
+	err := http.ListenAndServe(":8090", mux)
+	if err != nil {
+		log.Fatalf("server failed: %v", err)
+	}
 }
