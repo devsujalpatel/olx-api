1976be58ae4de495f067cb238d95d247cec3c092
Sujal Patel <sujal7455@gmail.com>
2026-08-31 15:23:28 +0530

    server struct created

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,19 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
-	_ "net/http/pprof"
+	"time"
 )
 
 func main() {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request){
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`{"status": "ok" }`))
 	})
 
-	err := http.ListenAndServe(":8090", mux)
-	if err != nil {
+	 srv := http.Server{
+		Addr: ":8090",
+		Handler: mux,
+		ReadTimeout: time.Second * 10,
+		WriteTimeout: time.Second * 30,
+		IdleTimeout: time.Second * 60,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("server failed: %v", err)
 	}
 }
